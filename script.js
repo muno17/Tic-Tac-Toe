7eb766bfde4ce490aefe4b9b6b6d88dc3d05dbf5
muno17 <munozd17@gmail.com>
2023-07-06 19:56:10 -0400

    add board, buttons to start game, footer

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 const Gameboard = (() => {
 
+    // add logic to prevent adding used spots
 
 })();
 
 const Flow = (() => {
 
+    // check when game is over
+    // congratulate winning player
 
 })();
 
 
 const Player = () => {
-
+    //add marks to spots on the board
 };
+
+
+// button to start/restart
+
+// create a computer player
