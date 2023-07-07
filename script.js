d831eb820a5d0976ccfa1e547272b22897b27cad
muno17 <munozd17@gmail.com>
2023-07-07 09:20:46 -0400

    add functionality to add x marks to board

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 const Gameboard = (() => {
+    // create array of tiles
+    let board = document.getElementsByClassName('tile');
+    console.log(board);
+    for (let i = 0; i < 9; i++) {
+        board[i].addEventListener('click', () => {
+            if (board[i] != 'X' || board[i] != 'O') {
+                board[i].innerHTML = "X";
+            }
+            
+        })
+    }
 
+    const reset = () => {
+        console.log("reset");
+        for (let i = 0; i < 9; i++) {
+            board[i].innerHTML = "";
+        }
+    };
+
+    return {reset};
     // add logic to prevent adding used spots
-
 })();
 
 const Flow = (() => {
@@ -18,5 +36,10 @@ const Player = () => {
 
 
 // button to start/restart
+let reset = document.getElementById('reset');
+reset.addEventListener('click', () => {
+    console.log('reset clicked');
+    Gameboard.reset;
+})
 
 // create a computer player
