dea45d29c977d339db59d744b9b88d3cb358cd80
muno17 <munozd17@gmail.com>
2023-07-11 09:58:08 -0400

    clean up js

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ const Gameboard = (() => {
     // create array of tiles
     let board = document.getElementsByClassName('tile');
     console.log(board);
+
     for (let i = 0; i < 9; i++) {
         board[i].addEventListener('click', () => {
-            if (board[i].innerHTML != 'X' && board[i].innerHTML != 'O') {
-                board[i].innerHTML = 'X';
-                Flow.nextTurn();
+            if (Flow.startGame()) {
+                if (board[i].innerHTML != 'X' && board[i].innerHTML != 'O') {
+                    board[i].innerHTML = 'X';
+                    Flow.nextTurn();
+                }
             }
             
         })
@@ -27,11 +30,13 @@ const Flow = (() => {
     // display
     let display = document.getElementById("gameDisplay");
     let turn;
+    let gameStart = false;
 
     let start = document.getElementById("start");
     start.addEventListener("click", () => {
         display.innerHTML = "Player 1's Turn";
         this.turn = 1;
+        gameStart = true;
     });
 
     let reset = document.getElementById("reset");
@@ -50,9 +55,13 @@ const Flow = (() => {
         display.innerHTML = `Player ${this.turn}\'s Turn`;
     };
 
+    const startGame = () => {
+        return gameStart;
+    };
+
     // check when game is over
     // congratulate winning player
-    return {nextTurn};
+    return {nextTurn, startGame};
 })();
 
 
